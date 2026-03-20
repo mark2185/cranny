a9a9f58aa29f90ffbcbfd59cf765ecba993a9011
Luka Markušić <markusicluka@gmail.com>
2026-03-20 22:23:08 +0100

    Working HTTP server on the phone

--- from-scratch/cranny.zig
+++ from-scratch/cranny.zig
@@ -11,21 +11,49 @@ extern fn __android_log_print(prio: c_int, tag: [*c]const u8, text: [*c]const u8
 
 const std = @import("std");
 
-fn logI(tag: [*c]const u8, text: [*c]const u8) void {
-    _ = __android_log_write(4, tag, text);
+fn LOGI(text: [*c]const u8) void {
+    _ = __android_log_write(4, "MANUAL_TAG_ZIG", text);
 }
+
+// TODO: how to implement a variadic?
+// LOGFI(text: [*c]const u8, ...) void {
+// _ = __android_log_print(4, "MANUAL_TAG_ZIG", text,
+// }
 
 export fn zig_main() callconv(.c) void {
     _ = __android_log_write(4, "MANUAL_TAG_ZIG", "Hello from zig_main!");
 
-    const address = std.net.Address.parseIp4("127.0.0.1", 7979) catch {
-        logI("MANUAL_TAG_ZIG", "Could not parse IPv4");
+    const address = std.net.Address.parseIp4("0.0.0.0", 7979) catch {
+        LOGI("Could not parse IPv4");
         return;
     };
 
     var server = address.listen(.{}) catch {
-        logI("MANUAL_TAG_ZIG", "Error listening!");
+        LOGI("Error listening!");
         return;
     };
     defer server.deinit();
+
+    while (true) {
+        handleConnection(server.accept() catch {
+            LOGI("Error accepting connection!");
+            return;
+        }) catch {
+            LOGI("Error handling a connection!");
+            return;
+        };
+        LOGI("Connection successful!");
+    }
+}
+
+fn handleConnection(conn: std.net.Server.Connection) !void {
+    defer conn.stream.close();
+    var buffer: [1024]u8 = undefined;
+
+    var reader = conn.stream.reader(&buffer);
+    var data_slices = [1][]u8{&buffer};
+    _ = reader.interface().readVec(&data_slices) catch {
+        LOGI("Error reading from connection stream");
+        return;
+    };
 }
