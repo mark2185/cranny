86a010568dac611cd986d2a732857435e959eaaf
Luka Markušić <markusicluka@gmail.com>
2026-03-20 14:45:01 +0100

    Transfer control over to zig

--- from-scratch/cranny.c
+++ from-scratch/cranny.c
@@ -1,5 +1,9 @@
 #include <android/log.h>
 
+void zig_main();
+
 void android_main(struct android_app* app) {
-    __android_log_write(ANDROID_LOG_INFO, "MANUAL_TAG", "Hello world!");
+    __android_log_write(ANDROID_LOG_INFO, "MANUAL_TAG", "Hello world from android_main!");
+    zig_main();
+    __android_log_write(ANDROID_LOG_INFO, "MANUAL_TAG", "Goodbye cruel world from android_main!");
 }
