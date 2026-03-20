e10e5dfac8b8b572beaff3d5bf7f9bbcb2e7800f
Luka Markušić <markusicluka@gmail.com>
2026-03-20 23:24:38 +0100

    Include log.h in zig

--- from-scratch/cranny.zig
+++ from-scratch/cranny.zig
@@ -1,18 +1,10 @@
-/// Writes the constant string text to the log, with priority prio and tag tag.
-/// Returns: 1 if the message was written to the log, or -EPERM if it was not; see __android_log_is_loggable().
-/// Source: https://developer.android.com/ndk/reference/group/logging
-extern fn __android_log_write(prio: c_int, tag: [*c]const u8, text: [*c]const u8) c_int;
-
-/// Writes a formatted string to the log, with priority prio and tag tag.
-/// The details of formatting are the same as for printf(3)
-/// Returns: 1 if the message was written to the log, or -EPERM if it was not; see __android_log_is_loggable().
-/// Source: https://man7.org/linux/man-pages/man3/printf.3.html
-extern fn __android_log_print(prio: c_int, tag: [*c]const u8, text: [*c]const u8, ...) c_int;
-
 const std = @import("std");
+const log = @cImport({
+    @cInclude("android/log.h");
+});
 
 fn LOGI(text: [*c]const u8) void {
-    _ = __android_log_write(4, "MANUAL_TAG_ZIG", text);
+    _ = log.__android_log_write(4, "MANUAL_TAG_ZIG", text);
 }
 
 // TODO: how to implement a variadic?
@@ -21,7 +13,7 @@ fn LOGI(text: [*c]const u8) void {
 // }
 
 export fn zig_main() callconv(.c) void {
-    _ = __android_log_write(4, "MANUAL_TAG_ZIG", "Hello from zig_main!");
+    _ = log.__android_log_write(4, "MANUAL_TAG_ZIG", "Hello from zig_main!");
 
     const address = std.net.Address.parseIp4("0.0.0.0", 7979) catch {
         LOGI("Could not parse IPv4");
