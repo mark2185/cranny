74a1960491ab0cfb4346daf7f35f3fecc8b103ee
Luka Markušić <markusicluka@gmail.com>
2026-03-22 17:08:27 +0100

    Avoid using C

--- from-scratch/cranny.c
+++ from-scratch/cranny.c
@@ -1,9 +1,59 @@
+#if 1
+#include <android/native_activity.h>
 #include <android/log.h>
+void onStart(ANativeActivity* activity) {
+    __android_log_write(ANDROID_LOG_INFO, "MANUAL_TAG", "Look at me, ma, I'm starting!");
+}
 
-void zig_main();
+void onPause(ANativeActivity* activity) {
+    __android_log_write(ANDROID_LOG_INFO, "MANUAL_TAG", "Time for a pause!");
+}
+
+void onResume(ANativeActivity* activity) {
+    __android_log_write(ANDROID_LOG_INFO, "MANUAL_TAG", "GUESS WHO'S BACK");
+}
+
+__attribute__((visibility("default")))
+void ANativeActivity_onCreate(ANativeActivity * activity, void* savedState, size_t savedStateSize) {
+    __android_log_write(ANDROID_LOG_INFO, "MANUAL_TAG", "I am filling up the callbacks of the activity");
+    activity->callbacks->onStart = onStart;
+    activity->callbacks->onResume = onResume;
+    activity->callbacks->onPause = onPause;
+}
+#else
+#include <android/log.h>
+#include <android/native_app_glue/android_native_app_glue.h>
+
+extern void zig_main();
+
+int32_t handle_input(struct android_app* app, AInputEvent* event)
+{
+    __android_log_write(ANDROID_LOG_INFO, "MANUAL_TAG", "Responding to a handle input!");
+    return 0;
+}
+
+void handle_cmd(struct android_app* app, int32_t cmd) {
+    __android_log_write(ANDROID_LOG_INFO, "MANUAL_TAG", "Responding to an event!");
+    switch (cmd) {
+        case APP_CMD_PAUSE:
+            __android_log_write(ANDROID_LOG_INFO, "MANUAL_TAG", "Responding to a pause event!");
+            break;
+    }
+}
 
 void android_main(struct android_app* app) {
     __android_log_write(ANDROID_LOG_INFO, "MANUAL_TAG", "Hello world from android_main!");
-    zig_main();
+    app->onAppCmd = handle_cmd;
+    app->onInputEvent = handle_input;
+    while (1) {
+        struct android_poll_source * source;
+        const int res = ALooper_pollAll(0, NULL, NULL, &source);
+        switch (res) {
+            case ALOOPER_POLL_CALLBACK:
+                break;
+        }
+    }
+    /* zig_main(); */
     __android_log_write(ANDROID_LOG_INFO, "MANUAL_TAG", "Goodbye cruel world from android_main!");
 }
+#endif
--- from-scratch/cranny.zig
+++ from-scratch/cranny.zig
@@ -3,8 +3,18 @@ const log = @cImport({
     @cInclude("android/log.h");
 });
 
+// const native_activity = @cImport({
+// @cInclude("android/native_activity.h");
+// });
+
 fn LOGI(text: [*c]const u8) void {
     _ = log.__android_log_write(4, "MANUAL_TAG_ZIG", text);
+}
+
+const ANativeActivity = struct {};
+
+export fn ANativeActivity_onCreate(_: *ANativeActivity, _: *anyopaque, _: u32) void {
+    LOGI("Hello from ANativeActivity_onCreate!");
 }
 
 // TODO: how to implement a variadic?
@@ -12,8 +22,9 @@ fn LOGI(text: [*c]const u8) void {
 // _ = __android_log_print(4, "MANUAL_TAG_ZIG", text,
 // }
 
-export fn zig_main() callconv(.c) void {
-    _ = log.__android_log_write(4, "MANUAL_TAG_ZIG", "Hello from zig_main!");
+fn zig_main() callconv(.c) void {
+    LOGI("Hello from zig_main!");
+    // log.__android_log_print(4, "MANUAL_TAG_ZIG", "Sdk version: %d", native_activity.ANativeActivity.sdkVersion);
 
     const address = std.net.Address.parseIp4("0.0.0.0", 7979) catch {
         LOGI("Could not parse IPv4");
@@ -27,8 +38,10 @@ export fn zig_main() callconv(.c) void {
     defer server.deinit();
 
     while (true) {
+        LOGI("Waiting for a connection on port 7979!");
+        // std.Thread.sleep(100_000_000);
         handleConnection(server.accept() catch {
-            LOGI("Error accepting connection!");
+            LOGI("Error accepting connection on port 7979!");
             return;
         }) catch {
             LOGI("Error handling a connection!");
