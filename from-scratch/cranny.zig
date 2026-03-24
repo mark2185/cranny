f068b92d99349f966f3e48302c091f407f684933
Luka Markušić <markusicluka@gmail.com>
2026-03-24 23:02:02 +0100

    Have a server accepting connections

--- from-scratch/cranny.zig
+++ from-scratch/cranny.zig
@@ -3,51 +3,104 @@ const log = @cImport({
     @cInclude("android/log.h");
 });
 
-// const native_activity = @cImport({
-// @cInclude("android/native_activity.h");
-// });
+const native_activity = @cImport({
+    @cInclude("android/native_activity.h");
+});
+const input = @cImport({
+    @cInclude("android/input.h");
+});
 
 fn LOGI(text: [*c]const u8) void {
     _ = log.__android_log_write(4, "MANUAL_TAG_ZIG", text);
 }
 
-const ANativeActivity = struct {};
+fn LOGFI(text: [*c]const u8, ...) callconv(.c) void {
+    var ap = @cVaStart();
+    defer @cVaEnd(&ap);
 
-export fn ANativeActivity_onCreate(_: *ANativeActivity, _: *anyopaque, _: u32) void {
-    LOGI("Hello from ANativeActivity_onCreate!");
+    _ = log.__android_log_print(4, "MANUAL_TAG_ZIG", text, @cVaArg(&ap, [*c]const u8));
 }
 
-// TODO: how to implement a variadic?
-// LOGFI(text: [*c]const u8, ...) void {
-// _ = __android_log_print(4, "MANUAL_TAG_ZIG", text,
-// }
+const ANativeWindow = extern struct {};
+const ARect = extern struct {};
 
-fn zig_main() callconv(.c) void {
-    LOGI("Hello from zig_main!");
-    // log.__android_log_print(4, "MANUAL_TAG_ZIG", "Sdk version: %d", native_activity.ANativeActivity.sdkVersion);
+const ANativeActivityCallbacks = extern struct {
+    onStart: *const fn (*ANativeActivity) callconv(.c) void,
+    onResume: *const fn (*ANativeActivity) callconv(.c) void,
+    onSaveInstance: *const fn (*ANativeActivity, usize) callconv(.c) *void,
+    onPause: *const fn (*ANativeActivity) callconv(.c) void,
+    onStop: *const fn (*ANativeActivity) callconv(.c) void,
+    onDestroy: *const fn (*ANativeActivity) callconv(.c) void,
+    onWindowFocusChanged: *const fn (*ANativeActivity, c_int) callconv(.c) void,
+    onNativeWindowCreated: *const fn (*ANativeActivity, *ANativeWindow) callconv(.c) void,
+    onNativeWindowResized: *const fn (*ANativeActivity, *ANativeWindow) callconv(.c) void,
+    onNativeWindowRedrawNeeded: *const fn (*ANativeActivity, *ANativeWindow) callconv(.c) void,
+    onNativeWindowDestroyed: *const fn (*ANativeActivity, *ANativeWindow) callconv(.c) void,
+    onInputQueueCreated: *const fn (*ANativeActivity, *input.AInputQueue) callconv(.c) void,
+    onInputQueueDestroyed: *const fn (*ANativeActivity, *input.AInputQueue) callconv(.c) void,
+    onContentRectChanged: *const fn (*ANativeActivity, *const ARect) callconv(.c) void,
+    onConfigurationChanged: *const fn (*ANativeActivity) callconv(.c) void,
+    onLowMemory: *const fn (*ANativeActivity) callconv(.c) void,
+};
 
+const ANativeActivity = extern struct {
+    callbacks: *ANativeActivityCallbacks,
+};
+
+var http_thread: ?std.Thread = null;
+
+var server_running = false;
+
+fn runServer() void {
+    LOGI("+runServer()");
     const address = std.net.Address.parseIp4("0.0.0.0", 7979) catch {
         LOGI("Could not parse IPv4");
         return;
     };
 
-    var server = address.listen(.{}) catch {
-        LOGI("Error listening!");
+    var server = address.listen(.{ .reuse_address = true, .force_nonblocking = true }) catch {
+        LOGI("Error listening on the address!");
         return;
     };
     defer server.deinit();
-
-    while (true) {
-        LOGI("Waiting for a connection on port 7979!");
-        // std.Thread.sleep(100_000_000);
-        handleConnection(server.accept() catch {
-            LOGI("Error accepting connection on port 7979!");
+    while (server_running) {
+        handleConnection(server.accept() catch |err| {
+            switch (err) {
+                // No connection was made which means the call to accept would block
+                error.WouldBlock => continue,
+                else => LOGI("Uh-oh, an actual error occurred!"),
+            }
             return;
         }) catch {
             LOGI("Error handling a connection!");
             return;
         };
         LOGI("Connection successful!");
+    }
+    LOGI("-runServer()");
+}
+
+fn onStart(_: *ANativeActivity) callconv(.c) void {
+    LOGI("Hello from onStart!");
+
+    if (http_thread == null) {
+        LOGI("Creating a thread!");
+        server_running = true;
+        http_thread = std.Thread.spawn(.{}, runServer, .{}) catch {
+            LOGI("Could not spawn a thread");
+            return;
+        };
+    }
+}
+
+fn onStop(_: *ANativeActivity) callconv(.c) void {
+    LOGI("Hello from onStop!");
+    LOGI("Spawned a thread!");
+    if (http_thread) |t| {
+        server_running = false;
+        t.join();
+        LOGI("HTTP server thread joined");
+        http_thread = null;
     }
 }
 
@@ -61,4 +114,42 @@ fn handleConnection(conn: std.net.Server.Connection) !void {
         LOGI("Error reading from connection stream");
         return;
     };
+
+    LOGFI("Got from the server: %s", &buffer);
+}
+
+fn onPause(_: *ANativeActivity) callconv(.c) void {
+    // LOGI("pausing");
+}
+
+fn onResume(_: *ANativeActivity) callconv(.c) void {
+    // LOGI("GUESS WHO'S BACK");
+}
+
+var q: *input.AInputQueue = undefined;
+
+fn onInputQueueCreated(_: *ANativeActivity, input_queue: *input.AInputQueue) callconv(.c) void {
+    // LOGI("Input queue created");
+    q = input_queue;
+}
+
+// extern fn AInputQueue_hasEvents(_: *input.AInputQueue) i32;
+
+export fn ANativeActivity_onCreate(activity: *ANativeActivity, _: *anyopaque, _: usize) void {
+    LOGI("Hello from ANativeActivity_onCreate!");
+    activity.callbacks.onStart = onStart;
+    activity.callbacks.onStop = onStop;
+    activity.callbacks.onPause = onPause;
+    activity.callbacks.onResume = onResume;
+    // activity.callbacks.onInputQueueCreated = onInputQueueCreated;
+
+    // while (true) {
+    // var event: *AInputEvent = undefined;
+    // if (q) {
+    // LOGI("input queue is a nullptr");
+    // } else if (input.AInputQueue_hasEvents(q) == 1) {
+    // LOGI("An event has been logged!");
+    // }
+    // }
+    LOGI("Goodbye from ANativeActivity_onCreate!");
 }
