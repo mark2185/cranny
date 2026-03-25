4c79931786bf01da2f2fb11b251d6c40c8ee6897
Luka Markušić <markusicluka@gmail.com>
2026-03-25 11:28:47 +0100

    Parse the HTTP request using std.Http
    Also ditch the zig versions of C structs for android

--- from-scratch/cranny.zig
+++ from-scratch/cranny.zig
@@ -24,35 +24,14 @@ fn LOGFI(text: [*c]const u8, ...) callconv(.c) void {
 const ANativeWindow = extern struct {};
 const ARect = extern struct {};
 
-const ANativeActivityCallbacks = extern struct {
-    onStart: *const fn (*ANativeActivity) callconv(.c) void,
-    onResume: *const fn (*ANativeActivity) callconv(.c) void,
-    onSaveInstance: *const fn (*ANativeActivity, usize) callconv(.c) *void,
-    onPause: *const fn (*ANativeActivity) callconv(.c) void,
-    onStop: *const fn (*ANativeActivity) callconv(.c) void,
-    onDestroy: *const fn (*ANativeActivity) callconv(.c) void,
-    onWindowFocusChanged: *const fn (*ANativeActivity, c_int) callconv(.c) void,
-    onNativeWindowCreated: *const fn (*ANativeActivity, *ANativeWindow) callconv(.c) void,
-    onNativeWindowResized: *const fn (*ANativeActivity, *ANativeWindow) callconv(.c) void,
-    onNativeWindowRedrawNeeded: *const fn (*ANativeActivity, *ANativeWindow) callconv(.c) void,
-    onNativeWindowDestroyed: *const fn (*ANativeActivity, *ANativeWindow) callconv(.c) void,
-    onInputQueueCreated: *const fn (*ANativeActivity, *input.AInputQueue) callconv(.c) void,
-    onInputQueueDestroyed: *const fn (*ANativeActivity, *input.AInputQueue) callconv(.c) void,
-    onContentRectChanged: *const fn (*ANativeActivity, *const ARect) callconv(.c) void,
-    onConfigurationChanged: *const fn (*ANativeActivity) callconv(.c) void,
-    onLowMemory: *const fn (*ANativeActivity) callconv(.c) void,
-};
-
-const ANativeActivity = extern struct {
-    callbacks: *ANativeActivityCallbacks,
-};
-
 var http_thread: ?std.Thread = null;
 
 var server_running = false;
 
 fn runServer() void {
     LOGI("+runServer()");
+    defer LOGI("-runServer()");
+
     const address = std.net.Address.parseIp4("0.0.0.0", 7979) catch {
         LOGI("Could not parse IPv4");
         return;
@@ -77,14 +56,32 @@ fn runServer() void {
         };
         LOGI("Connection successful!");
     }
-    LOGI("-runServer()");
 }
 
-fn onStart(_: *ANativeActivity) callconv(.c) void {
-    LOGI("Hello from onStart!");
+fn onStart(_: [*c]native_activity.ANativeActivity) callconv(.c) void {
+    LOGI("+onStart()");
+    defer LOGI("-onStart()");
+    // LOGI("Incrementing the number in the file test.txt");
+
+    // const file_path = "/storage/emulated/0/Android/data/com.manual.apk/files/test.txt";
+
+    // create it if it doesn't exist
+    // var file = std.fs.createFileAbsolute(file_path, .{ .truncate = false, .read = true }) catch |err| {
+    // LOGI(@errorName(err));
+    // LOGI("Early return!");
+    // return;
+    // };
+    // defer file.close();
+
+    // read it and increment it
+    // var buffer: [1024]u8 = @splat(0);
+    // var reader = file.reader(&buffer);
+    // var data_slices = [1][]u8{&buffer};
+    // const read_bytes = reader.interface.readVec(&data_slices) catch 0;
+    // LOGFI("Read bytes: %d", read_bytes);
 
     if (http_thread == null) {
-        LOGI("Creating a thread!");
+        LOGI("Spawning a thread!");
         server_running = true;
         http_thread = std.Thread.spawn(.{}, runServer, .{}) catch {
             LOGI("Could not spawn a thread");
@@ -93,9 +90,9 @@ fn onStart(_: *ANativeActivity) callconv(.c) void {
     }
 }
 
-fn onStop(_: *ANativeActivity) callconv(.c) void {
-    LOGI("Hello from onStop!");
-    LOGI("Spawned a thread!");
+fn onStop(_: [*c]native_activity.ANativeActivity) callconv(.c) void {
+    LOGI("+onStop()");
+    defer LOGI("-onStop()");
     if (http_thread) |t| {
         server_running = false;
         t.join();
@@ -106,42 +103,32 @@ fn onStop(_: *ANativeActivity) callconv(.c) void {
 
 fn handleConnection(conn: std.net.Server.Connection) !void {
     defer conn.stream.close();
-    var buffer: [1024]u8 = undefined;
 
-    var reader = conn.stream.reader(&buffer);
-    var data_slices = [1][]u8{&buffer};
-    _ = reader.interface().readVec(&data_slices) catch {
-        LOGI("Error reading from connection stream");
+    var recv_buffer: [1024]u8 = undefined;
+    var send_buffer: [1024]u8 = undefined;
+
+    var connection_br = conn.stream.reader(&recv_buffer);
+    var connection_bw = conn.stream.writer(&send_buffer);
+
+    var server = std.http.Server.init(connection_br.interface(), &connection_bw.interface);
+    const request = server.receiveHead() catch {
+        LOGI("Unable to receive head from the HTTP request");
         return;
     };
 
-    LOGFI("Got from the server: %s", &buffer);
-}
-
-fn onPause(_: *ANativeActivity) callconv(.c) void {
-    // LOGI("pausing");
-}
-
-fn onResume(_: *ANativeActivity) callconv(.c) void {
-    // LOGI("GUESS WHO'S BACK");
+    switch (request.head.method) {
+        .GET => LOGI("Got a GET request!"),
+        .POST => LOGI("Got a POST request!"),
+        else => LOGI("Got something else"),
+    }
 }
 
 var q: *input.AInputQueue = undefined;
 
-fn onInputQueueCreated(_: *ANativeActivity, input_queue: *input.AInputQueue) callconv(.c) void {
-    // LOGI("Input queue created");
-    q = input_queue;
-}
-
-// extern fn AInputQueue_hasEvents(_: *input.AInputQueue) i32;
-
-export fn ANativeActivity_onCreate(activity: *ANativeActivity, _: *anyopaque, _: usize) void {
+export fn ANativeActivity_onCreate(activity: [*c]native_activity.ANativeActivity, _: *anyopaque, _: usize) void {
     LOGI("Hello from ANativeActivity_onCreate!");
-    activity.callbacks.onStart = onStart;
-    activity.callbacks.onStop = onStop;
-    activity.callbacks.onPause = onPause;
-    activity.callbacks.onResume = onResume;
-    // activity.callbacks.onInputQueueCreated = onInputQueueCreated;
+    activity.*.callbacks.*.onStart = onStart;
+    activity.*.callbacks.*.onStop = onStop;
 
     // while (true) {
     // var event: *AInputEvent = undefined;
